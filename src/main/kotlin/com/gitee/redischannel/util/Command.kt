9a1c502deca79e2d7be5024dff6f75a51a30d874
zhibei <785740487@qq.com>
2025-05-28 05:46:27 +0800

    Revert "fixAPI"
    This reverts commit 493dbbaa356f9d80036968801f780e5563681f02.

--- src/main/kotlin/com/gitee/redischannel/util/Command.kt
+++ src/main/kotlin/com/gitee/redischannel/util/Command.kt
@@ -1,34 +1,31 @@
 package com.gitee.redischannel.util
 
-import com.gitee.redischannel.RedisChannelPlugin.Type.CLUSTER
-import com.gitee.redischannel.RedisChannelPlugin.Type.SINGLE
-import com.gitee.redischannel.RedisChannelPlugin.type
 import com.gitee.redischannel.api.RedisChannelAPI
 import com.gitee.redischannel.api.cluster.RedisClusterCommandAPI
 import com.gitee.redischannel.api.RedisCommandAPI
 import com.gitee.redischannel.api.RedisPubSubAPI
 import com.gitee.redischannel.api.cluster.RedisClusterPubSubAPI
-import com.gitee.redischannel.core.ClusterRedisManager
-import com.gitee.redischannel.core.RedisManager
+
+/**
+ * 获取集群命令API
+ * @throws ClassCastException 当你使用的不是集群模式时
+ * */
+fun RedisChannelAPI.clusterCommandAPI(): RedisClusterCommandAPI = this as RedisClusterCommandAPI
 
 /**
  * 获取命令API
+ * @throws ClassCastException 当你使用的是集群模式时
  * */
-fun RedisChannelAPI.commandAPI(): RedisCommandAPI {
-    val manager = when (type) {
-        CLUSTER -> ClusterRedisManager
-        SINGLE -> RedisManager
-    }
-    return manager as RedisCommandAPI
-}
+fun RedisChannelAPI.commandAPI(): RedisCommandAPI = this as RedisCommandAPI
+
+/**
+ * 获取集群发布/订阅API
+ * @throws ClassCastException 当你使用的不是集群模式时
+ * */
+fun RedisChannelAPI.clusterPubSubAPI(): RedisClusterPubSubAPI = this as RedisClusterPubSubAPI
 
 /**
  * 获取发布/订阅API
+ * @throws ClassCastException 当你使用的是集群模式时
  * */
-fun RedisChannelAPI.pubSubAPI(): RedisPubSubAPI {
-    val manager = when (type) {
-        CLUSTER -> ClusterRedisManager
-        SINGLE -> RedisManager
-    }
-    return manager as RedisPubSubAPI
-}
+fun RedisChannelAPI.pubSubAPI(): RedisPubSubAPI = this as RedisPubSubAPI
